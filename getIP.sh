b44618c35609b502d45c1025ac1c47a3c0dd8489
Sebull-git <sebastian.weber04@gmx.net>
2022-08-12 21:38:53 +0200

    slight changes and connect tool

--- getIP.sh
+++ getIP.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
-docker inspect $1 | gron | grep NetworkSettings.Networks.bridge.IPAddress | cut -d "=" -f2 | cut -d '"' -f2
+list=$(docker ps --format "{{.Names}}")
+
+while IFS= read -r line; do
+    ip=$(docker inspect $line | gron | grep NetworkSettings.Networks.bridge.IPAddress | cut -d "=" -f2 | cut -d '"' -f2)
+    echo "$line:$ip"
+done <<< "$list"
